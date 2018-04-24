3a647c2694026dbdd2f40db0013efc2d464a3246
dennis <duan2720@mylaurier.ca>
2018-04-24 13:19:42 -0400

    setup mlab and mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   googleClientID: '209700703971-cvd5erbobr19o4d1crv3jen9f7m2ub5o.apps.googleusercontent.com',
-  googleClientSecret: 'Ad1VR6EU6i5GhXWej9RHgR5S'
+  googleClientSecret: 'Ad1VR6EU6i5GhXWej9RHgR5S',
+  mongoURI: 'mongodb://coinmarketcap_dennis:coinmarketcap@ds245287.mlab.com:45287/coinmarketcap_ubiq'
 }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-require('./services/passport')
-;
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
